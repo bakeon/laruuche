c63bfdd6a56c552b9c5504b9102fbdaee1e81e79
alexandrepol <alexandre.pol1996@gmail.com>
2017-07-15 00:32:05 +0200

    scrollBottom directive

--- app/scripts/controllers/user/mymentors.js
+++ app/scripts/controllers/user/mymentors.js
@@ -43,7 +43,7 @@ angular.module('laruucheApp')
             let myRoom = firebase.database().ref('chatrooms').child('private').orderByChild('uid').equalTo(mentorId+studentId);
             myRoom.on('value', function(snap){
               for(let value in snap.val()){
-                $location.path('/panel/privateroom/'+value);
+                $location.path('/userProfile/privateroom/'+value);
                 break;
               }
             });
